a1a774a28ac3661101dee6d5bd83de492f0ff4c4
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-08-07 17:33:27 -0300

    validando por regex no campo celular

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from clientes.models import Cliente
-from clientes.validators import cpd_valido, nome_valido, rg_valido
+from clientes.validators import cpd_valido, nome_valido, rg_valido, validate_celular
 
 
 class ClienteSerializer(serializers.ModelSerializer):
@@ -17,9 +17,7 @@ class ClienteSerializer(serializers.ModelSerializer):
 
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'RG': 'O RG  deve ter 9 Digitos'})
-        return data
 
-    # def validate_celular(self, celular):
-    #     if len(celular) != 11:
-    #         raise serializers.ValidationError('O celular  deve ter 9 Digitos no Minimo')
-    #     return celular
+        if not validate_celular(data['celular']):
+            raise serializers.ValidationError({'celular': 'O numero deve seguir o Padrao: (11 12345-1234)'})
+        return data
--- clientes/validators.py
+++ clientes/validators.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+import re
 
 
 def cpd_valido(numero_cpf):
@@ -11,6 +12,13 @@ def nome_valido(nome):
 
 def rg_valido(numero_rg):
     return len(numero_rg) == 9
+
+
+def validate_celular(numero_celular):
+    """Verifica se o celular valido (35 99808-1515)"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    return re.findall(modelo, numero_celular)
+
 
 
 
